f5bbe62cc496585af1a90d761320c30700133f30
Plynio Henrique <plyniohenrique@gmail.com>
2021-11-16 21:39:07 -0300

    Adição de 5 características

--- src/app/mock-student.ts
+++ src/app/mock-student.ts
@@ -1,14 +1,14 @@
 import { Student } from './student';
 
 export const STUDENT: Student[] = [
-  { id: 1, name: 'Rayla' },
-  { id: 2, name: 'Barry' },
-  { id: 3, name: 'Pikachu' },
-  { id: 4, name: 'Mark' },
-  { id: 5, name: 'Peter' },
-  { id: 6, name: 'Rafael' },
-  { id: 7, name: 'Matheus' },
-  { id: 8, name: 'Leticia' },
-  { id: 9, name: 'Jessica' },
-  { id: 10, name: 'Plynio' }
+  { id: 1, name: 'Rayla', sex: 'female', age: 20, address: 'Rua pikachu 123', blood: 'o+', cpf: '456.579.586-59' },
+  { id: 2, name: 'Barry', sex: 'male', age: 35, address: 'Rua pikachu 123', blood: 'o-', cpf: '456.579.586-59' },
+  { id: 3, name: 'Pikachu', sex: 'male', age: 7, address: 'Rua pikachu 123', blood: 'o+', cpf: '456.579.586-59' },
+  { id: 4, name: 'Mark', sex: 'male', age: 40, address: 'Rua pikachu 123', blood: 'o-', cpf: '456.579.586-59' },
+  { id: 5, name: 'Peter', sex: 'male', age: 27, address: 'Rua pikachu 123', blood: 'a+', cpf: '456.579.586-59' },
+  { id: 6, name: 'Rafael', sex: 'male', age: 20, address: 'Rua pikachu 123', blood: 'o-', cpf: '456.579.586-59' },
+  { id: 7, name: 'Matheus', sex: 'female', age: 24, address: 'Rua pikachu 123', blood: 'o+', cpf: '456.579.586-59' },
+  { id: 8, name: 'Leticia', sex: 'female', age: 21, address: 'Rua pikachu 123', blood: 'ab+', cpf: '456.579.586-59' },
+  { id: 9, name: 'Jessica', sex: 'female', age: 19, address: 'Rua pikachu 123', blood: 'o+', cpf: '456.579.586-59' },
+  { id: 10, name: 'Plynio', sex: 'male', age: 20, address: 'Rua pikachu 123', blood: 'o+', cpf: '456.579.586-59' }
 ];
--- src/app/student.ts
+++ src/app/student.ts
@@ -1,4 +1,9 @@
 export interface Student {
     id: number;
     name: string;
+    sex: string;
+    age: number;
+    address: string;
+    blood: string;
+    cpf: string;
   }
